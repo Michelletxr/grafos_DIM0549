8d6349880339d818a342addc545738dfbfda2819
Michelle Teixeira <michelleteixeiramartins@gmail.com>
2024-03-09 16:57:47 -0300

    add algoritmo de busca em profundidade

--- algoritmos/search_algorithms.py
+++ algoritmos/search_algorithms.py
@@ -17,3 +17,34 @@ def BFS(graph:Graph, raiz) -> Graph:
                 graph_result.add_aresta(v,w) # (n)
     
     return graph_result
+
+def check_consecutivos(lista, elemento1, elemento2):
+    #print("pilha", lista)
+    # Verifica se os elementos estão na lista
+    if elemento1 in lista and elemento2 in lista:
+        # Obtém os índices dos elementos na lista
+        indice1 = lista.index(elemento1)
+        indice2 = lista.index(elemento2)
+        # Verifica se os índices são consecutivos
+        if abs(indice1 - indice2) == 1:
+            return True
+    return False
+
+
+
+def DFS(graph: Graph, tree, pilha: list, raiz) -> Graph:
+    tree.add_vertice(raiz)
+    pilha.append(raiz)
+    #print("|--", raiz)
+    for w in graph.neighbors(raiz):
+        if w not in tree.vertices():
+            tree.add_vertice(w)
+            tree.add_aresta(raiz, w)
+            #print("adicionando aresta:", (raiz,w))
+            tree = DFS(graph=graph, tree=tree, pilha=pilha, raiz=w)
+        elif w in pilha and not check_consecutivos(pilha, raiz, w):
+            #aresta de retorno
+            #print("adicionando aresta de retorno:", (raiz, w))
+            tree.add_aresta(raiz, w)
+    pilha.remove(raiz)
+    return tree
